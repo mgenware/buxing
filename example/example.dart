79f01b358738d293a4bb203ced20fbef064f0aae
Mgen <m@mgenware.com>
2021-11-15 00:08:59 +0800

    Download a static file as an example

--- example/example.dart
+++ example/example.dart
@@ -3,7 +3,8 @@
 import 'package:buxing/buxing.dart';
 
 void main() async {
-  var task =
-      Task('https://www.apache.org/foundation/', 'test.html', logging: true);
+  var task = Task(
+      'https://www.apache.org/img/asf-estd-1999-logo.jpg', 'test.jpg',
+      logging: true);
   await task.start();
 }
