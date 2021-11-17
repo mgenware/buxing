c7c78448e7d8e26bc549b3214ca53833e16b4889
Mgen <m@mgenware.com>
2021-11-18 01:21:25 +0800

    Use mock_byte_stream to mock TPWConn

--- example/example.dart
+++ example/example.dart
@@ -1,9 +1,9 @@
 import 'package:buxing/buxing.dart';
+import 'package:buxing/src/logger.dart';
 
 void main() async {
   var task = Task(
-      Uri.parse('https://www.apache.org/img/asf-estd-1999-logo.jpg'),
-      'test.jpg',
-      logging: true);
+      Uri.parse('https://golang.org/dl/go1.17.3.src.tar.gz'), 'a.zip',
+      worker: ParallelWorker(), logger: Logger(level: LogLevel.info));
   await task.start();
 }
