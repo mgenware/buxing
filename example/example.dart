83faff6cb2c0b5737a82de3e8f5b72b26f2d864c
Mgen <m@mgenware.com>
2021-11-21 14:54:45 +0800

    Update example

--- example/example.dart
+++ example/example.dart
@@ -2,8 +2,8 @@ import 'package:buxing/buxing.dart';
 import 'package:buxing/src/logger.dart';
 
 void main() async {
-  var task = Task(
-      Uri.parse('https://coldfunction.com/dds/ua/103/50_nNZqOS.png'), 'a.png',
-      logger: Logger());
+  var task = Task(Uri.parse('https://golang.org/dl/go1.17.3.src.tar.gz'),
+      'downloads/go1.17.3.src.tar.gz',
+      worker: ParallelWorker(), logger: Logger());
   await task.start();
 }
